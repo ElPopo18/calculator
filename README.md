956f7f3b5a087317626cdd50dea17c8641d6a2c6
ElPopo18 <70975283+ElPopo18@users.noreply.github.com>
2025-07-21 11:44:36 -0400

    Add the basic math functions
    Will add the functions of the main math operations (addition, subtraction, multiplication and division)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # calculator
 A calculator project using Javascript
+
+The final project of the Foundations course of the Odin project.
+
+In this challenge i will have to use all the tools that i learned during the course to 
+make a calculator, the main tools i will be using are the following:
+
+HTML
+CSS
+Javascript
---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let num1 = 10;
+let num2 = 5;
+
+addition(num1, num2);
+subtraction(num1, num2);
+multiplication(num1, num2);
+division(num1, num2);
+
+function addition(num1, num2){
+    console.log (num1 + num2);
+}
+
+function subtraction(num1, num2){
+    console.log (num1 - num2);
+}
+
+function multiplication(num1, num2){
+    console.log (num1 * num2);
+}
+
+function division(num1, num2){
+    console.log (num1 / num2);
+}
