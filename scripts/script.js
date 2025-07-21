b658e76a800c116930a0bc24bddab9eb470df5ad
ElPopo18 <70975283+ElPopo18@users.noreply.github.com>
2025-07-21 15:49:44 -0400

    Add the structure and design of the Calculator
    Will add the base HTML structure and CSS styles of the calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,44 @@
-let num1 = 10;
-let num2 = 5;
-let operator = '/';
+document.addEventListener('DOMContentLoaded', function () {
+    let num1 = 10;
+    let num2 = 5;
+    let operator = '/';
 
-operate(num1, num2, operator);
+    operate(num1, num2, operator);
 
-function operate(num1, num2, operator){
-    switch (operator){
-        case '+':
-            addition(num1, num2);
-            break;
-        case '-':
-            subtraction(num1, num2);
-            break;
-        case '*':
-            multiplication(num1, num2);
-            break;
-        case '/':
-            division(num1, num2);
-            break;
+    function operate(num1, num2, operator){
+        switch (operator){
+            case '+':
+                addition(num1, num2);
+                break;
+            case '-':
+                subtraction(num1, num2);
+                break;
+            case '*':
+                multiplication(num1, num2);
+                break;
+            case '/':
+                division(num1, num2);
+                break;
+        }
     }
-}
 
-function addition(num1, num2){
-    console.log (num1 + num2);
-}
+    function addition(num1, num2){
+        console.log (num1 + num2);
+    }
 
-function subtraction(num1, num2){
-    console.log (num1 - num2);
-}
+    function subtraction(num1, num2){
+        console.log (num1 - num2);
+    }
 
-function multiplication(num1, num2){
-    console.log (num1 * num2);
-}
+    function multiplication(num1, num2){
+        console.log (num1 * num2);
+    }
 
-function division(num1, num2){
-    console.log (num1 / num2);
-}
+    function division(num1, num2){
+        console.log (num1 / num2);
+    }
+
+
+
+
+})
