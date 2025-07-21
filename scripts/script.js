ae28961a68518dede3683a8e5e0a0ff306228faf
ElPopo18 <70975283+ElPopo18@users.noreply.github.com>
2025-07-21 17:44:45 -0400

    Add functionality to the number's and operator's buttons
    Will add functionality to all the button of the operators and numbers

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,42 @@ document.addEventListener('DOMContentLoaded', function () {
     let num1 = 10;
     let num2 = 5;
     let operator = '/';
+    let display = document.querySelector(".display")
+
+    const numbers = {
+        one: document.querySelector(".one"),
+        two: document.querySelector(".two"),
+        three: document.querySelector(".three"),
+        four: document.querySelector(".four"),
+        five: document.querySelector(".five"),
+        six: document.querySelector(".six"),
+        seven: document.querySelector(".seven"),
+        eight: document.querySelector(".eight"),
+        nine: document.querySelector(".nine"),
+        cero: document.querySelector(".cero"),
+    }
+
+    const operators = {
+        addition: document.querySelector(".addition"),
+        subtraction: document.querySelector(".subtraction"),
+        multiplication: document.querySelector(".multiplication"),
+        division: document.querySelector(".division"),
+    }
+
+    Object.values(operators).forEach(btn => {
+        btn.addEventListener("click", function printOperators() {
+            display.textContent = btn.textContent;
+        })
+    })
+    
+
+    Object.values(numbers).forEach(btn => {
+        btn.addEventListener("click", function printNumbers() {
+            display.textContent = btn.textContent;
+        })
+    })
+
+    console.log(display.textContent);
 
     operate(num1, num2, operator);
 
