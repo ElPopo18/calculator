51b284bea2a0a2c18abe0eb3f4241d9f467f71cc
ElPopo18 <70975283+ElPopo18@users.noreply.github.com>
2025-07-22 12:12:05 -0400

    Add the function to fully display the screen
    Will add the functionality of displaying the first number, the operator and the second number.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     let firstNumber = '';
-    let num2 = 5;
+    let secondNumber = '';
     let operator = '';
     let globalCounter = 0;
-    let display = document.querySelector(".display")
-
-    if(operator === true){
-        console.log(true);
-    } else if (operator === false){
-        console.log(false);
-    }
+    let display = document.querySelector(".display");
+    const numerical = '0123456789';
+    const operatorsList = '+-*/';
 
     const numbers = {
         one: document.querySelector(".one"),
@@ -31,38 +27,48 @@ document.addEventListener('DOMContentLoaded', function () {
         division: document.querySelector(".division"),
     }
 
-    Object.values(operators).forEach(btn => {
+    Object.values(operators).forEach(btn => {  //Will convert the values of the parameters of the operators object into an array
         btn.addEventListener("click", function printOperators() {
-            if(display.textContent.length == 1 || display.textContent.length == 2){
+            /* Will convert display.textContent into an array, take the last element of it
+            and convert it into another array, then it will filter that array and check if it
+            contains a number or a operator, if it contains a number then it will execute the conditional
+            and print the operator in the display screen, if not then it will not print anything */
+            let filteredOperator = display.textContent.split('')
+            .slice(-1)
+            .filter((character) => numerical.includes(character))
+            if(filteredOperator.length == 1 && operator == ''){
+                console.log(filteredOperator);
                 globalCounter ++;
                 operator = btn.textContent;
-                display.textContent += operator;
-                console.log(operator);
-                console.log(globalCounter);
+                display.textContent += operator
             }
         })
     })
     
     Object.values(numbers).forEach(btn => {
         btn.addEventListener("click", function printNumbers() {
-            let numberPlaceholder = btn.textContent;
-                globalCounter ++;
+            if(globalCounter === 0 || globalCounter === 1){
+                let numberPlaceholder = btn.textContent;
+                    globalCounter++;
+                    display.textContent += numberPlaceholder;
+                    firstNumber = display.textContent;
+            }
+            /* Will convert display.textContent into an array, take the last element of it
+            and convert it into another array, then it will filter that array and check if it
+            contains a number or a operator, if it contains a operator then it will execute the conditional
+            and print the numbers in the display screen, if not then it will not print anything */
+            let filteredNumbers = display.textContent.split('')
+            .slice(-2)
+            .filter((character) => operatorsList.includes(character));
+            if(globalCounter >= 2 && filteredNumbers.length > 0){
+                let numberPlaceholder = btn.textContent;
                 display.textContent += numberPlaceholder;
-                firstNumber = display.textContent;
-                console.log(globalCounter);
-            
+                secondNumber = display.textContent;
+            }
         })
     })
 
-    if(globalCounter >= 2){
-        Object.values(numbers).forEach(btn => {
-            btn.addEventListener("click", function printNumbers() {
-                let numberPlaceholder = btn.textContent;
-                    display.textContent += numberPlaceholder;
-                    firstNumber = display.textContent;
-            })
-        })
-    }
+
 
     operate(firstNumber, num2, operator);
 
@@ -98,8 +104,5 @@ document.addEventListener('DOMContentLoaded', function () {
     function division(firstNumber, num2){
         console.log (firstNumber / num2);
     }
-
-
-
 
 })
